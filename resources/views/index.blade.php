13178d077ade1e150ac7d20e5ab0d69ef9930a91
Meyy17 <qilaadikara@gmail.com>
2023-09-13 12:42:03 +0700

    style [MY] slicing login

---
+++ resources/views/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PPOS RUS</title>
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}" />
+</head>
+
+<body>
+    <div class="bg">
+        <div class="img-bg">
+            <div class="center-wrapper">
+                <div class="dashboard-wrapper">
+                    <div class="iconsgroup">
+                        <img class="logob" src="{{asset('assets/image/logob.svg')}}">
+                        <div class="centerlogof">
+                            <img class="logof" src="{{asset('assets/image/logof.svg')}}">
+                        </div>
+                    </div>
+                    <h1 style="text-align: center; color: white;">
+                        PEMILIHAN PEMIMPIN ORGANISASI SEKOLAH<br>SMK RADEN UMAR SAID KUDUS<br>2023-2024
+                    </h1>
+                    <div class="card">
+                        <span style="color: white;">Masukkan NIS</span>
+                        <input type="number" placeholder="NIS">
+                        <button>Masuk</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
